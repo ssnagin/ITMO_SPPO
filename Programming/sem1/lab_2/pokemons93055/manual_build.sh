fb3e2b8d49feda32c63771685957d61459c54b86
ssnagin <56646530+ssnagin@users.noreply.github.com>
2024-11-23 13:07:44 +0300

    321

--- Programming/sem1/lab_2/pokemons93055/manual_build.sh
+++ Programming/sem1/lab_2/pokemons93055/manual_build.sh
@@ -1,2 +1,2 @@
 javac -classpath lib/*:. -d target -sourcepath . src/pokemons93055/Main.java src/pokemons93055/pokemons/*.java src/pokemons93055/move/physical/*.java src/pokemons93055/move/status/*.java src/pokemons93055/move/special/*.java
-jar -cmf mf.mf build.jar -C out target/
+jar -cvf mf.mf Build/Build.jar lib/* -C target/ .
